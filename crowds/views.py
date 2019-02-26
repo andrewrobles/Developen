b4219b912f98eb19c554e9f57fc3a00beb2f2602
andrewrobles <andrewrobles@berkeley.edu>
2019-02-26 08:12:38 -0800

    Switch from hardcoding to templating system.

--- crowds/views.py
+++ crowds/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the crowds index.")
+    return render(request, 'crowds/index.html')
