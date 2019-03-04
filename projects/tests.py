43a69362f498ee9ca2bdb3b3c176dc6591b09c2f
andrewrobles <andrewrobles@berkeley.edu>
2019-03-03 16:40:47 -0800

    Write write to test if username in database is set after submitting a create account form.

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,12 @@
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+class UserAccountTests(TestCase):
+	def test_create_user(self):
+		form_data = {'firstname': 'Amani', 'lastname': 'Sidhant', 'username': 'amansidhant', 'password': 'top_secret'}
+		self.client.post('/projects/create-account/', form_data)
+
+		user = User.objects.get(username='amansidhant')
+
+		self.assertEqual(user.username, 'amansidhant')
