8a33f8ceedcd58bb9d429fe22e55db7acd406df7
andrewrobles <andrewrobles@berkeley.edu>
2019-03-04 14:58:26 -0800

    Create Delete User Test.

--- projects/tests.py
+++ projects/tests.py
@@ -1,12 +1,38 @@
 
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+
+from .views import delete_account
 
 class UserAccountTests(TestCase):
+	def setUp(self):
+		create_account_form_data = {
+			'firstname': 'Oski', 
+			'lastname': 'Bear', 
+			'username': 'oskibear', 
+			'password': 'top_secret'
+		}
+
+		self.factory = RequestFactory()
+		self.client.post('/projects/create-account/', create_account_form_data)
+		
+
 	def test_create_user(self):
-		form_data = {'firstname': 'Amani', 'lastname': 'Sidhant', 'username': 'amansidhant', 'password': 'top_secret'}
-		self.client.post('/projects/create-account/', form_data)
+		user = User.objects.get(username='oskibear')
 
-		user = User.objects.get(username='amansidhant')
+		self.assertEqual(user.username, 'oskibear')
 
-		self.assertEqual(user.username, 'amansidhant')
+	def test_delete_user(self):
+		user = User.objects.get(username='oskibear')
+		request = self.factory.get('/projects/delete-account/')
+		request.user = user
+
+		delete_account(request)
+		user_list = User.objects.filter(username='oskibear')
+
+		self.assertEqual(len(user_list), 0) 
+
+
+
+
+
